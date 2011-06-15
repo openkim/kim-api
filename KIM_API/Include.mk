46941925424a8c684a2dd48d7b1d4b98cf62bbf8
Ryan S. Elliott <elliott@aem.umn.edu>
2011-06-15 11:27:33 -0500

    line 64-67 changed default

--- KIM_API/Include.mk
+++ KIM_API/Include.mk
@@ -54,17 +54,17 @@ else
    OBJONLY=-c
    OUTPUTIN=-o
    FORTRANFLAG = -fcray-pointer -O3 -J$(KIM_API_DIR) -D $(MACHINESYSTEM)
-   CCOMPILER   = gcc
-   CPPCOMPILER = g++
-   #CCOMPILER   = gcc-fsf-4.4  # for OS X using fink compilers
-   #CPPCOMPILER = g++-fsf-4.4  # for OS X using fink compilers
+   #CCOMPILER   = gcc
+   #CPPCOMPILER = g++
+   CCOMPILER   = gcc-fsf-4.4  # for OS X using fink compilers
+   CPPCOMPILER = g++-fsf-4.4  # for OS X using fink compilers
    CPPFLAG = -O3 -I$(KIM_API_DIR) -Wno-write-strings -D KIM_DIR_MODELS=\"$(KIM_MODELS_DIR)\" -D KIM_DIR_API=\"$(KIM_API_DIR)\"
    CPPFLAG += -D KIM_DIR_TESTS=\"$(KIM_TESTS_DIR)\"
    FORTRANCOMPILER = gfortran
-   #CPPLIBFLAG = -lgfortran           #if GNU version 4.5 and up. tested on suse 
-   #LINKCOMPILER = $(CPPCOMPILER)     #if GNU version 4.5 and up. tested on suse
-   CPPLIBFLAG = -lstdc++             #if GNU version 4.4.1. tested on suse
-   LINKCOMPILER = $(FORTRANCOMPILER) #if GNU version 4.4.1. tested on suse
+   CPPLIBFLAG = -lgfortran           #if GNU version 4.5 and up. tested on suse 
+   LINKCOMPILER = $(CPPCOMPILER)     #if GNU version 4.5 and up. tested on suse
+   #CPPLIBFLAG = -lstdc++             #if GNU version 4.4.1. tested on suse
+   #LINKCOMPILER = $(FORTRANCOMPILER) #if GNU version 4.4.1. tested on suse
    ifdef KIM_DYNAMIC
       CPPLIBFLAG += -ldl
    endif
