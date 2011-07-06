da82bf59ea7780cce53cd6a2daa79af0c2ec779f
Ryan S. Elliott <elliott@aem.umn.edu>
2011-07-06 14:40:12 -0500

    updated INSTALL docs, added definition of Rij vectors, removed fink compile lines from Include.mk

--- KIM_API/Include.mk
+++ KIM_API/Include.mk
@@ -56,8 +56,6 @@ else
    FORTRANFLAG = -fcray-pointer -O3 -I$(KIM_API_DIR) -D $(MACHINESYSTEM)
    CCOMPILER   = gcc
    CPPCOMPILER = g++
-   #CCOMPILER   = gcc-fsf-4.4  # for OS X using fink compilers
-   #CPPCOMPILER = g++-fsf-4.4  # for OS X using fink compilers
    CPPFLAG = -O3 -I$(KIM_API_DIR) -Wno-write-strings -D KIM_DIR_MODELS=\"$(KIM_MODELS_DIR)\" -D KIM_DIR_API=\"$(KIM_API_DIR)\"
    CPPFLAG += -D KIM_DIR_TESTS=\"$(KIM_TESTS_DIR)\"
    FORTRANCOMPILER = gfortran
