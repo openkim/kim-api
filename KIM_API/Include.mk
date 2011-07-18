2b4d278a1eebb06c43226abcf9a10510a2dfab7e
Valeriu Smirichinski <smirvi@linux-skp9.site>
2011-07-18 15:24:02 -0500

    gnu default compiler back to gnu (was open mpi)

--- KIM_API/Include.mk
+++ KIM_API/Include.mk
@@ -54,16 +54,16 @@ else
    OBJONLY=-c
    OUTPUTIN=-o
    FORTRANFLAG = -fcray-pointer -O3 -I$(KIM_API_DIR) -D $(MACHINESYSTEM)
-   #CCOMPILER   = gcc
-   #CPPCOMPILER = g++
-   CCOMPILER   = mpicc
-   CPPCOMPILER = mpiCC
+   CCOMPILER   = gcc
+   CPPCOMPILER = g++
+   #CCOMPILER   = mpicc
+   #CPPCOMPILER = mpiCC
    #CCOMPILER   = gcc-fsf-4.4  # for OS X using fink compilers
    #CPPCOMPILER = g++-fsf-4.4  # for OS X using fink compilers
    CPPFLAG = -O3 -I$(KIM_API_DIR) -Wno-write-strings -D KIM_DIR_MODELS=\"$(KIM_MODELS_DIR)\" -D KIM_DIR_API=\"$(KIM_API_DIR)\"
    CPPFLAG += -D KIM_DIR_TESTS=\"$(KIM_TESTS_DIR)\"
-   #FORTRANCOMPILER = gfortran
-   FORTRANCOMPILER = mpif90
+   FORTRANCOMPILER = gfortran
+   #FORTRANCOMPILER = mpif90
    CPPLIBFLAG = -lgfortran           #if GNU version 4.5 and up. tested on suse 
    LINKCOMPILER = $(CPPCOMPILER)     #if GNU version 4.5 and up. tested on suse
    #CPPLIBFLAG = -lstdc++             #if GNU version 4.4.1. tested on suse
