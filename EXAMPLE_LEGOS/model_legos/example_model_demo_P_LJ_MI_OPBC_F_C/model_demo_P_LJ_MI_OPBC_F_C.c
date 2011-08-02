98327faa04452f8c46549a8cad84f795e8adae24
Ryan S. Elliott <elliott@aem.umn.edu>
2011-08-02 08:55:50 -0500

    added place holders for new models and tests to be filled in by Valeriu

---
+++ EXAMPLE_LEGOS/model_legos/example_model_demo_P_LJ_MI_OPBC_F_C/model_demo_P_LJ_MI_OPBC_F_C.c
@@ -0,0 +1,4 @@
+/* Model Initiation routine */
+void model_demo_p_lj_mi_opbc_f_c_init_(void * km){
+
+}
---
+++ EXAMPLE_LEGOS/model_legos/example_model_demo_P_LJ_MI_OPBC_H_C/model_demo_P_LJ_MI_OPBC_H_C.c
@@ -0,0 +1,4 @@
+/* Model Initiation routine */
+void model_demo_p_lj_mi_opbc_h_c_init_(void * km){
+
+}
