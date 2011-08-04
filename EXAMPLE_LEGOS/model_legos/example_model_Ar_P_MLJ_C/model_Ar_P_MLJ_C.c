c9d9745a49a39c3611f2930550a62535d2dbe6c0
Ryan S. Elliott <elliott@aem.umn.edu>
2011-08-03 20:59:55 -0500

    added model_Ar_P_MLJ_C

---
+++ EXAMPLE_LEGOS/model_legos/example_model_Ar_P_MLJ_C/model_Ar_P_MLJ_C.c
@@ -0,0 +1,620 @@
+/*******************************************************************************
+*
+*  model_Ar_P_MLJ_C
+*
+*  Modified LJ pair potential (with smooth cutoff) Model for Ar
+*
+*  Authors: Valeriu Smirichinski, Ryan S. Elliott, Ellad B. Tadmor
+*
+*  Language: C
+*
+*  Copyright 2011 Ellad B. Tadmor, Ryan S. Elliott, and James P. Sethna
+*  All rights reserved.
+*
+*******************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "KIMserviceC.h"
+
+/******************************************************************************
+* Below are the definitions and values of all Model parameters
+*******************************************************************************/
+#define DIM 3       /* dimensionality of space */
+#define SPECCODE 1  /* internal species code */
+#define MODEL_CUTOFF 8.15 /* cutoff radius in angstroms */
+#define MODEL_CUTSQ  (MODEL_CUTOFF * MODEL_CUTOFF)
+#define SIGMA 3.40
+#define EPSILON 0.0104
+#define CUTNORM (MODEL_CUTOFF/SIGMA)
+#define CUTNORM3 (CUTNORM*CUTNORM*CUTNORM)
+#define CUTNORM6 (CUTNORM3*CUTNORM3)
+#define CUTNORM14 (CUTNORM6*CUTNORM6*CUTNORM*CUTNORM)
+#define APARAM (12.0*EPSILON*(-26.0 + 7.0*CUTNORM6)/(CUTNORM14*SIGMA*SIGMA))
+#define BPARAM (96.0*EPSILON*(7.0 - 2.0*CUTNORM6)/(CUTNORM6*CUTNORM6*CUTNORM*SIGMA))
+#define CPARAM (28.0*EPSILON*(-13.0 + 4.0*CUTNORM6)/(CUTNORM6*CUTNORM6))
+
+
+/* Define prototypes for model init */
+/* must be all lowercase to be compatible with the KIM API (to support Fortran Tests) */
+/**/
+void model_ar_p_mlj_c_init_(void* km);
+
+/* Define prototypes for model reinit, compute, and destroy */
+/* defined as static to avoid namespace clashes with other Models */
+/**/
+static void compute(void* km, int* ier);
+static void report_error(int line, char* str, int status);
+/**/
+static void calc_phi(double r, double* phi);
+static void calc_phi_dphi(double r, double* phi, double* dphi);
+
+
+/* Calculate pair potential phi(r) */
+static void calc_phi(double r, double* phi)
+{
+   /* local variables */
+   double rsq   = r*r;
+   double sor   = SIGMA/r;
+   double sor6  = sor*sor*sor*sor*sor*sor;
+   double sor12 = sor6*sor6;
+   
+   if (r > MODEL_CUTOFF)
+   {
+      /* Argument exceeds cutoff radius */
+      *phi = 0.0;
+   }
+   else
+   {
+      *phi = 4.0*EPSILON*(sor12 - sor6) + APARAM*rsq + BPARAM*r + CPARAM;
+   }
+
+   return;
+}
+
+/* Calculate pair potential phi(r) and its derivative dphi(r) */
+static void calc_phi_dphi(double r, double* phi, double* dphi)
+{
+   /* local variables */
+   double rsq   = r*r;
+   double sor   = SIGMA/r;
+   double sor6  = sor*sor*sor*sor*sor*sor;
+   double sor12 = sor6*sor6;
+
+   if (r > MODEL_CUTOFF)
+   {
+      /* Argument exceeds cutoff radius */
+      *phi  = 0.0;
+      *dphi = 0.0;
+   }
+   else
+   {
+      *phi  = 4.0*EPSILON*(sor12 - sor6) + APARAM*rsq + BPARAM*r + CPARAM;
+      *dphi = 24.0*EPSILON*(-2.0*sor12 + sor6)/r + 2.0*APARAM*r + BPARAM;
+   }
+
+   return;
+}
+
+/* compute function */
+static void compute(void* km, int* ier)
+{
+   /* local static parameters */
+   const static cutsq = MODEL_CUTOFF * MODEL_CUTOFF;
+   
+   /* local variables */
+   intptr_t* pkim = *((intptr_t**) km);
+   double R;
+   double Rsqij;
+   double phi;
+   double dphi;
+   double Rij[DIM];
+   int i;
+   int j;
+   int jj;
+   int k;
+   int numOfAtomNeigh;
+   int requestedAtom;
+   int currentAtom;
+   int comp_force;
+   int comp_energyPerAtom;
+   int comp_virial;
+   int IterOrLoca;
+   int HalfOrFull;
+   int NBC;
+   char* NBCstr;
+
+   intptr_t* nAtoms;
+   int* atomTypes;
+   double* Rij_list;
+   double* coords;
+   double* energy;
+   double* force;
+   double* energyPerAtom;
+   double* virial;
+   int* neighListOfCurrentAtom;
+   double* boxlength;
+   
+   
+   /* Determine neighbor list boundary condition (NBC) */
+   /* and half versus full mode: */
+   /*****************************
+    * HalfOrFull = 1 -- Half
+    *            = 2 -- Full
+    *****************************/
+   NBCstr = KIM_API_get_NBC_method(pkim, ier);
+   if (1 > *ier)
+   {
+      report_error(__LINE__, "KIM_API_get_NBC_method", *ier);
+      return;
+   }
+   if (!strcmp("CLUSTER",NBCstr))
+   {	   
+      NBC = 0;
+      HalfOrFull = 1;
+   }
+   else if (!strcmp("MI-OPBC-H",NBCstr))
+   {	   
+      NBC = 1;
+      HalfOrFull = 1;
+   }
+   else if (!strcmp("MI-OPBC-F",NBCstr))
+   {	   
+      NBC = 1;
+      HalfOrFull = 2;
+   }
+
+   else if (!strcmp("NEIGH-PURE-H",NBCstr))
+   {	   
+      NBC = 2;
+      HalfOrFull = 1;
+   }
+   else if (!strcmp("NEIGH-PURE-F",NBCstr))
+   {	   
+      NBC = 2;
+      HalfOrFull = 2;
+   }
+   else if (!strcmp("NEIGH-RVEC-F",NBCstr))
+   {	   
+      NBC = 3;
+      HalfOrFull = 2;
+   }
+   else
+   {
+      *ier = 0;
+      report_error(__LINE__, "Unknown NBC method", *ier);
+      return;
+   }
+   free(NBCstr); /* don't forget to release the memory... */
+
+   /* determine neighbor list handling mode */
+   if (NBC != 0)
+   {
+      /*****************************
+       * IterOrLoca = 1 -- Iterator
+       *            = 2 -- Locator
+       *****************************/
+      IterOrLoca = KIM_API_get_neigh_mode(pkim, ier);
+      if (1 > *ier)
+      {
+         report_error(__LINE__, "KIM_API_get_neigh_mode", *ier);
+         return;
+      }
+      if ((IterOrLoca != 1) && (IterOrLoca != 2))
+      {
+         printf("* ERROR: Unsupported IterOrLoca mode = %i\n", IterOrLoca);
+         exit(-1);
+      }
+   }
+   else
+   {
+      IterOrLoca = 2;   /* for CLUSTER NBC */
+   }
+
+   /* check to see if we have been asked to compute the forces, energyPerAtom, and virial */
+   comp_force = KIM_API_isit_compute(pkim, "forces", ier);
+   if (1 > *ier)
+   {
+      report_error(__LINE__, "KIM_API_isit_compute", *ier);
+      return;
+   }
+   comp_energyPerAtom = KIM_API_isit_compute(pkim, "energyPerAtom", ier);
+   if (1 > *ier)
+   {
+      report_error(__LINE__, "KIM_API_isit_compute", *ier);
+      return;
+   }
+   comp_virial = KIM_API_isit_compute(pkim, "virial", ier);
+   if (1 > *ier)
+   {
+      report_error(__LINE__, "KIM_API_isit_compute", *ier);
+      return;
+   }
+
+
+   /* unpack data from KIM object */
+   nAtoms = (intptr_t*) KIM_API_get_data(pkim, "numberOfAtoms", ier);
+   if (1 > *ier)
+   {
+      report_error(__LINE__, "KIM_API_get_data", *ier);
+      return;
+   }
+   atomTypes= (int*) KIM_API_get_data(pkim, "atomTypes", ier);
+   if (1 > *ier)
+   {
+      report_error(__LINE__, "KIM_API_get_data", *ier);
+      return;
+   }
+   energy = (double*) KIM_API_get_data(pkim, "energy", ier);
+   if (1 > *ier)
+   {
+      report_error(__LINE__, "KIM_API_get_data", *ier);
+      return;
+   }
+   coords = (double*) KIM_API_get_data(pkim, "coordinates", ier);
+   if (1 > *ier)
+   {
+      report_error(__LINE__, "KIM_API_get_data", *ier);
+      return;
+   }
+   if (NBC == 1)
+   {
+      boxlength = (double*) KIM_API_get_data(pkim, "boxlength", ier);
+      if (1 > *ier)
+      {
+         report_error(__LINE__, "KIM_API_get_data", *ier);
+         return;
+      }
+   }
+
+   if (comp_force)
+   {
+      force = (double*) KIM_API_get_data(pkim, "forces", ier);
+      if (1 > *ier)
+      {
+         report_error(__LINE__, "KIM_API_get_data", *ier);
+         return;
+      }
+   }
+
+   if (comp_energyPerAtom)
+   {
+      energyPerAtom = (double*) KIM_API_get_data(pkim, "energyPerAtom", ier);
+      if (1 > *ier)
+      {
+         report_error(__LINE__, "KIM_API_get_data", *ier);
+         return;
+      }
+   }
+
+   if (comp_virial)
+   {
+      virial = (double*) KIM_API_get_data(pkim, "virial", ier);
+      if (1 > *ier)
+      {
+         report_error(__LINE__, "KIM_API_get_data", *ier);
+         return;
+      }
+   }
+
+   /* Check to be sure that the atom types are correct */
+   /**/
+   *ier = 0; /* assume an error */
+   for (i = 0; i < *nAtoms; ++i)
+   {
+      if ( SPECCODE != atomTypes[i])
+      {
+         report_error(__LINE__, "Unexpected species type detected", i);
+         return;
+      }
+   }
+   *ier = 1; /* everything is ok */
+
+   /* initialize potential energies, forces, and virial term */
+   if (comp_energyPerAtom)
+   {
+      for (i = 0; i < *nAtoms; ++i)
+      {
+         energyPerAtom[i] = 0.0;
+      }
+   }
+   else
+   {
+      *energy = 0.0;
+   }
+
+   if (comp_force)
+   {
+      for (i = 0; i < *nAtoms; ++i)
+      {
+         for (k = 0; k < DIM; ++k)
+         {
+            force[i*DIM + k] = 0.0;
+         }
+      }
+   }
+
+   if (comp_virial)
+   {
+      *virial = 0.0;
+   }
+
+   /* Initialize neighbor handling for CLUSTER NBC */
+   if (0 == NBC) /* CLUSTER */
+   {
+      neighListOfCurrentAtom = (int *) malloc((*nAtoms)*sizeof(int));
+   }
+
+   /* Initialize neighbor handling for Iterator mode */
+
+   if (1 == IterOrLoca)
+   {
+      if (1 == HalfOrFull) /* HALF list */
+      {
+         *ier = KIM_API_get_half_neigh(pkim, 0, 0, &currentAtom, &numOfAtomNeigh,
+                                       &neighListOfCurrentAtom, &Rij_list);
+      }
+      else                 /* FULL list */
+      {
+         *ier = KIM_API_get_full_neigh(pkim, 0, 0, &currentAtom, &numOfAtomNeigh,
+                                       &neighListOfCurrentAtom, &Rij_list);
+      }
+      /* check for successful initialization */
+      if (2 != *ier) /* ier == 2 upon successful initialization */
+      {
+         if (1 == HalfOrFull)
+         {
+            report_error(__LINE__, "KIM_API_get_half_neigh", *ier);
+         }
+         else
+         {
+            report_error(__LINE__, "KIM_API_get_full_neigh", *ier);
+         }
+         ier = 0;
+         return;
+      }
+   }
+
+   /* Compute enery and forces */
+
+   /* loop over particles and compute enregy and forces */
+   i = 0;
+   while( 1 )
+   {
+
+      /* Set up neighbor list for next atom for all NBC methods */
+      if (1 == IterOrLoca) /* ITERATOR mode */
+      {
+         if (1 == HalfOrFull) /* HALF list */
+         {
+            *ier = KIM_API_get_half_neigh(pkim, 0, 1, &currentAtom, &numOfAtomNeigh,
+                                          &neighListOfCurrentAtom, &Rij_list);  
+         }
+         else
+         {
+            *ier = KIM_API_get_full_neigh(pkim, 0, 1, &currentAtom, &numOfAtomNeigh,
+                                          &neighListOfCurrentAtom, &Rij_list);
+         }
+         if (0 > *ier) /* some sort of problem, exit */
+         {
+            if (1 == HalfOrFull)
+            {
+               report_error(__LINE__, "KIM_API_get_half_neigh", *ier);
+            }
+            else
+            {
+               report_error(__LINE__, "KIM_API_get_full_neigh", *ier);
+            }
+            return;
+         }
+         if (0 == *ier) /* ier==0 means that the iterator has been incremented past */
+         {              /* the end of the list, terminate loop                      */
+            break;
+         }
+
+         i = currentAtom;
+      }
+      else
+      {
+         i += 1;
+         if (*nAtoms < i) /* incremented past end of list, terminate loop */
+         {
+            break;
+         }
+
+         if (1 == HalfOrFull) /* HALF list */
+         {
+            if (0 == NBC)     /* CLUSTER NBC method */
+            {
+               numOfAtomNeigh = *nAtoms - i;
+               for (k = 0; k < numOfAtomNeigh; ++k)
+               {
+                  neighListOfCurrentAtom[k] = i + k + 1;
+               }
+               *ier = 1;
+            }
+            else
+            {
+               *ier = KIM_API_get_half_neigh(pkim, 1, i, &currentAtom, &numOfAtomNeigh,
+                                             &neighListOfCurrentAtom, &Rij_list);
+            }
+         }
+         else                 /* FULL list */
+         {
+            *ier = KIM_API_get_full_neigh(pkim, 1, i, &currentAtom, &numOfAtomNeigh,
+                                          &neighListOfCurrentAtom, &Rij_list);
+         }
+      }
+      if (1 != *ier) /* some sort of problem, exit */
+      {
+         if (1 == HalfOrFull)
+         {
+            report_error(__LINE__, "KIM_API_get_half_neigh", *ier);
+         }
+         else
+         {
+            report_error(__LINE__, "KIM_API_get_full_neigh", *ier);
+         }
+         *ier = 0;
+         return;
+      }
+            
+      /* loop over the neighbors of atom i */
+      for (jj = 0; jj < numOfAtomNeigh; ++ jj)
+      {
+
+         j = neighListOfCurrentAtom[jj]; /* get neighbor ID */
+
+         /* compute relative position vector and squared distance */
+         Rsqij = 0.0;
+         for (k = 0; k < DIM; ++k)
+         {
+            if (3 != NBC) /* all methods except NEIGH-RVEC-F */
+            {
+               Rij[k] = coords[j*DIM + k] - coords[i*DIM + k];
+            }
+            else          /* NEIGH-RVEC-F method */
+            {
+               Rij[k] = Rij_list[jj*DIM + k];
+            }
+
+            /* apply periodic boundary conditions if required */
+            if (1 == NBC)
+            {
+               if (abs(Rij[k]) > 0.5*boxlength[k])
+               {
+                  Rij[k] -= (Rij[k]/fabs(Rij[k]))*boxlength[k];
+               }
+            }
+            
+            /* compute squared distance */
+            Rsqij += Rij[k]*Rij[k];
+         }
+         
+         /* compute energy and force */
+         if (Rsqij < MODEL_CUTSQ) /* particles are interacting ? */
+         {
+            R = sqrt(Rsqij);
+            if (comp_force || comp_virial)
+            {
+               /* compute pair potential and its derivative */
+               calc_phi_dphi(R, &phi, &dphi);
+               }
+            else
+            {
+               /* compute just pair potential */
+               calc_phi(R, &phi);
+            }
+            
+            /* contribution to energy */
+            if (comp_energyPerAtom)
+            {
+               energyPerAtom[i] += 0.5*phi;
+               /* if half list add energy for the other atom in the pair */
+               if (1 == HalfOrFull) energyPerAtom[j] += 0.5*phi;
+            }
+            else
+            {
+               if (1 == HalfOrFull)
+               {
+                  /* Half mode -- add v to total energy */
+                  *energy += phi;
+               }
+               else
+               {
+                  /* Full mode -- add half v to total energy */
+                  *energy += 0.5*phi;
+               }
+            }
+            
+            /* contribution to virial perssure */
+            if (comp_virial)
+            {
+               if (1 == HalfOrFull)
+               {
+                  /* Half mode -- varial = sum r*(dphi/dr) */
+                  *virial += R*dphi;
+               }
+               else
+               {
+                  /* Full mode -- varial = sum 0.6*r*(dphi/dr) */
+                  *virial += 0.5*R*dphi;
+               }
+            }
+            
+            /* contribution to forces */
+            if (comp_force)
+            {
+               for (k = 0; k < DIM; ++k)
+               {
+                  force[i*DIM + k] += dphi*Rij[k]/R; /* accumulate force on atom i */
+                  if (1 == HalfOrFull)
+                  {
+                     force[j*DIM + k] -= dphi*Rij[k]/R; /* Fji = -Fij */
+                  }
+               }
+            }
+         }
+      } /* loop on jj */
+   }    /* infinite while loop (terminated by break statements above */
+   
+
+   /* perform final tasks */
+   
+   if (comp_virial)
+   {
+      *virial = -*virial/( (double) DIM); /* definition of virial term */
+   }
+
+   if (comp_energyPerAtom)
+   {
+      *energy = 0.0;
+      for (k = 0; k < *nAtoms; ++k)
+      {
+         *energy += energyPerAtom[k];
+      }
+   }
+
+   /* Free temporary storage */
+   if (0 == NBC) 
+   {
+      free(neighListOfCurrentAtom);
+   }
+
+   /* everything is great */
+   *ier = 1;
+   return;
+}
+
+/* Initialization function */
+void model_ar_p_mlj_c_init_(void *km)
+{
+   /* Local variables */
+   intptr_t* pkim = *((intptr_t**) km);
+   double* model_cutoff;
+   int ier;
+
+   /* store pointer to compute function in KIM object */
+   if (! KIM_API_set_data(pkim, "compute", 1, (void*) &compute))
+   {
+      report_error(__LINE__, "KIM_API_set_data", ier);
+      exit(1);
+   }
+
+   /* store model cutoff in KIM object */
+   model_cutoff = (double*) KIM_API_get_data(pkim, "cutoff", &ier);
+   if (1 > ier)
+   {
+      report_error(__LINE__, "KIM_API_get_data", ier);
+      exit(1);
+   }
+   *model_cutoff = MODEL_CUTOFF; /* cutoff distance in angstroms */
+
+   return;
+}
+
+static void report_error(int line, char* str, int status)
+{
+   printf("* ERROR at line %i in %s: %s. kimerror = %i\n", line, __FILE__, str, status);
+}
