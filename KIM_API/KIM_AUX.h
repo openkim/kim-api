332c9d12a61d496fa21cff9bf6caa396fea8301a
Valeriu Smirichinski <smirvi@linux-skp9.site>
2011-11-11 16:54:33 -0600

    simplified loops for processing virialGlobal and per atom, no difference for half or full neighboring lists

--- KIM_API/KIM_AUX.h
+++ KIM_API/KIM_AUX.h
@@ -24,7 +24,7 @@ namespace KIM_Standard_Virials{
             virialGlobal = (double *) pkim->get_data("virialGlobal",&ierGlobal);
             virialPerAtom = (double *) pkim->get_data("virialPerAtom",&ierPerAtom);
             numberOfAtoms = (int *) pkim->get_data("numberOfAtoms",kimerr);
-            halfNeighbors = !pkim->requiresFullNeighbors();
+           // halfNeighbors = !pkim->requiresFullNeighbors();
             
             if (*kimerr !=KIM_STATUS_OK) return;
             if (ierGlobal == KIM_STATUS_OK && virialGlobal != NULL) {
@@ -49,76 +49,44 @@ namespace KIM_Standard_Virials{
             double *dx = *pdx;
             //v=(*de)/((*r)*(*r))/3.0;
             //v=(*de)/((*r)*(*r));
-            v=(*de);
-            if (halfNeighbors){
+            v=(*de)/(*r);
+  if (virialGlobal_flag ==1 && pkim->virialGlobal_need2add) {
+       vir[0] = v * dx[0] * dx[0];
+       vir[1] = v * dx[1] * dx[1];
+       vir[2] = v * dx[2] * dx[2];
+       vir[3] = v * dx[1] * dx[2];
+       vir[4] = v * dx[0] * dx[2];
+       vir[5] = v * dx[0] * dx[1];
+       virialGlobal[0] += vir[0];
+       virialGlobal[1] += vir[1];
+       virialGlobal[2] += vir[2];
+       virialGlobal[3] += vir[3];
+       virialGlobal[4] += vir[4];
+       virialGlobal[5] += vir[5];
+    }
+    if (virialPerAtom_flag==1 && pkim->virialPerAtom_need2add ){
 
-                vir[0] = v * dx[0] * dx[0];
-                vir[1] = v * dx[1] * dx[1];
-                vir[2] = v * dx[2] * dx[2];
-                vir[3] = v * dx[1] * dx[2];
-                vir[4] = v * dx[0] * dx[2];
-                vir[5] = v * dx[0] * dx[1];
+       vir[0] =0.5 * v * dx[0] * dx[0];
+       vir[1] =0.5 * v * dx[1] * dx[1];
+       vir[2] =0.5 * v * dx[2] * dx[2];
+       vir[3] =0.5 * v * dx[1] * dx[2];
+       vir[4] =0.5 * v * dx[0] * dx[2];
+       vir[5] =0.5 * v * dx[0] * dx[1];
+       virialPerAtom[(*i)*6 + 0] += vir[0];
+       virialPerAtom[(*i)*6 + 1] += vir[1];
+       virialPerAtom[(*i)*6 + 2] += vir[2];
+       virialPerAtom[(*i)*6 + 3] += vir[3];
+       virialPerAtom[(*i)*6 + 4] += vir[4];
+       virialPerAtom[(*i)*6 + 5] += vir[5];
 
-                if (virialGlobal_flag ==1 && pkim->virialGlobal_need2add) {
-                    virialGlobal[0] += vir[0];
-                    virialGlobal[1] += vir[1];
-                    virialGlobal[2] += vir[2];
-                    virialGlobal[3] += vir[3];
-                    virialGlobal[4] += vir[4];
-                    virialGlobal[5] += vir[5];
-                }
-                if (virialPerAtom_flag==1 && pkim->virialPerAtom_need2add ){
-                
-                    virialPerAtom[(*i)*6 + 0] += vir[0]*0.5;
-                    virialPerAtom[(*i)*6 + 1] += vir[1]*0.5;
-                    virialPerAtom[(*i)*6 + 2] += vir[2]*0.5;
-                    virialPerAtom[(*i)*6 + 3] += vir[3]*0.5;
-                    virialPerAtom[(*i)*6 + 4] += vir[4]*0.5;
-                    virialPerAtom[(*i)*6 + 5] += vir[5]*0.5;
+       virialPerAtom[(*j)*6 + 0] += vir[0];
+       virialPerAtom[(*j)*6 + 1] += vir[1];
+       virialPerAtom[(*j)*6 + 2] += vir[2];
+       virialPerAtom[(*j)*6 + 3] += vir[3];
+       virialPerAtom[(*j)*6 + 4] += vir[4];
+       virialPerAtom[(*j)*6 + 5] += vir[5];
+     }
 
-                    virialPerAtom[(*j)*6 + 0] += vir[0]*0.5;
-                    virialPerAtom[(*j)*6 + 1] += vir[1]*0.5;
-                    virialPerAtom[(*j)*6 + 2] += vir[2]*0.5;
-                    virialPerAtom[(*j)*6 + 3] += vir[3]*0.5;
-                    virialPerAtom[(*j)*6 + 4] += vir[4]*0.5;
-                    virialPerAtom[(*j)*6 + 5] += vir[5]*0.5;
-                }
-            }else{
-                v=v*0.5;
-                vir[0] = v * dx[0] * dx[0];
-                vir[1] = v * dx[1] * dx[1];
-                vir[2] = v * dx[2] * dx[2];
-                vir[3] = v * dx[1] * dx[2];
-                vir[4] = v * dx[0] * dx[2];
-                vir[5] = v * dx[0] * dx[1];
-
-                if (virialGlobal_flag ==1 && pkim->virialGlobal_need2add) {
-                    virialGlobal[0] += vir[0];
-                    virialGlobal[1] += vir[1];
-                    virialGlobal[2] += vir[2];
-                    virialGlobal[3] += vir[3];
-                    virialGlobal[4] += vir[4];
-                    virialGlobal[5] += vir[5];
-                }
-                if (virialPerAtom_flag==1 && pkim->virialPerAtom_need2add ){
-
-                    virialPerAtom[(*i)*6 + 0] += vir[0];
-                    virialPerAtom[(*i)*6 + 1] += vir[1];
-                    virialPerAtom[(*i)*6 + 2] += vir[2];
-                    virialPerAtom[(*i)*6 + 3] += vir[3];
-                    virialPerAtom[(*i)*6 + 4] += vir[4];
-                    virialPerAtom[(*i)*6 + 5] += vir[5];
-                    
-                    if(*j >=  *numberOfAtoms){
-                        virialPerAtom[(*j)*6 + 0] += vir[0];
-                        virialPerAtom[(*j)*6 + 1] += vir[1];
-                        virialPerAtom[(*j)*6 + 2] += vir[2];
-                        virialPerAtom[(*j)*6 + 3] += vir[3];
-                        virialPerAtom[(*j)*6 + 4] += vir[4];
-                        virialPerAtom[(*j)*6 + 5] += vir[5];
-                    }
-                }
-            }
             *ier = KIM_STATUS_OK;  
     }
 }
