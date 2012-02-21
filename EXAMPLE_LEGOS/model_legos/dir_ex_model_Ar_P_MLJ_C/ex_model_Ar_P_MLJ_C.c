6c431a87db4253919865b95c164e96de06df9c8b
Ryan S. Elliott <elliott@aem.umn.edu>
2012-02-21 16:08:19 -0600

    Added CDDL header and updated copyright

--- EXAMPLE_LEGOS/model_legos/dir_ex_model_Ar_P_MLJ_C/ex_model_Ar_P_MLJ_C.c
+++ EXAMPLE_LEGOS/model_legos/dir_ex_model_Ar_P_MLJ_C/ex_model_Ar_P_MLJ_C.c
@@ -1,19 +1,45 @@
+/*
+*
+* CDDL HEADER START
+*
+* The contents of this file are subject to the terms of the Common Development
+* and Distribution License Version 1.0 (the "License").
+*
+* You can obtain a copy of the license at
+* http://www.opensource.org/licenses/CDDL-1.0.  See the License for the
+* specific language governing permissions and limitations under the License.
+*
+* When distributing Covered Code, include this CDDL HEADER in each file and
+* include the License file in a prominent location with the name LICENSE.CDDL.
+* If applicable, add the following below this CDDL HEADER, with the fields
+* enclosed by brackets "[]" replaced with your own identifying information:
+*
+* Portions Copyright (c) [yyyy] [name of copyright owner]. All rights reserved.
+*
+* CDDL HEADER END
+*
+
+*
+* Copyright (c) 2012, Regents of the University of Minnesota.  All rights reserved.
+*
+* Contributors:
+*    Ryan S. Elliott
+*    Ellad B. Tadmor
+*
+*/
+
 /*******************************************************************************
 *
 *  model_Ar_P_MLJ_C
 *
 *  Modified LJ pair potential (with smooth cutoff) Model for Ar
 *
-*  Authors: Valeriu Smirichinski, Ryan S. Elliott, Ellad B. Tadmor
-*
 *  Language: C
 *
 *  Release: This file is part of the openkim-api.git repository.
 *
-*  Copyright 2011 Ellad B. Tadmor, Ryan S. Elliott, and James P. Sethna
-*  All rights reserved.
-*
 *******************************************************************************/
+
 
 #include <stdlib.h>
 #include <stdio.h>
--- EXAMPLE_LEGOS/test_legos/dir_ex_test_demo_F03/neighborlistmod.F90
+++ EXAMPLE_LEGOS/test_legos/dir_ex_test_demo_F03/neighborlistmod.F90
@@ -1,10 +1,33 @@
 !
+! CDDL HEADER START
+!
+! The contents of this file are subject to the terms of the Common Development
+! and Distribution License Version 1.0 (the "License").
+!
+! You can obtain a copy of the license at
+! http://www.opensource.org/licenses/CDDL-1.0.  See the License for the
+! specific language governing permissions and limitations under the License.
+!
+! When distributing Covered Code, include this CDDL HEADER in each file and
+! include the License file in a prominent location with the name LICENSE.CDDL.
+! If applicable, add the following below this CDDL HEADER, with the fields
+! enclosed by brackets "[]" replaced with your own identifying information:
+!
+! Portions Copyright (c) [yyyy] [name of copyright owner]. All rights reserved.
+!
+! CDDL HEADER END
+!
+
+!
+! Copyright (c) 2012, Regents of the University of Minnesota.  All rights reserved.
+!
+! Contributors:
+!    Valeriu Smirichinski
+!    Yeranuhi Hakobyan
+!
+
+!
 ! Release: This file is part of the openkim-api.git repository.
-!
-! Copyright 2011 Ellad B. Tadmor, Ryan S. Elliott, and James P. Sethna
-! All rights reserved.
-!
-! Authors: Valeriu Smirichinski and Yeranuhi Hakobyan
 !
 
 
