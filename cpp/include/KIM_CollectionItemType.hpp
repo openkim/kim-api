199833b3ab988e5814509e1c8997f47b83f7df6a
Ryan S. Elliott <relliott@umn.edu>
2019-06-14 15:09:33 -0500

    Added Collection, CollectionItemType, and Collections C++ interfaces

---
+++ cpp/include/KIM_CollectionItemType.hpp
@@ -0,0 +1,215 @@
+//
+// CDDL HEADER START
+//
+// The contents of this file are subject to the terms of the Common Development
+// and Distribution License Version 1.0 (the "License").
+//
+// You can obtain a copy of the license at
+// http://www.opensource.org/licenses/CDDL-1.0.  See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// When distributing Covered Code, include this CDDL HEADER in each file and
+// include the License file in a prominent location with the name LICENSE.CDDL.
+// If applicable, add the following below this CDDL HEADER, with the fields
+// enclosed by brackets "[]" replaced with your own identifying information:
+//
+// Portions Copyright (c) [yyyy] [name of copyright owner]. All rights reserved.
+//
+// CDDL HEADER END
+//
+
+//
+// Copyright (c) 2016--2019, Regents of the University of Minnesota.
+// All rights reserved.
+//
+// Contributors:
+//    Ryan S. Elliott
+//
+
+//
+// Release: This file is part of the kim-api.git repository.
+//
+
+
+#ifndef KIM_COLLECTION_ITEM_TYPE_HPP_
+#define KIM_COLLECTION_ITEM_TYPE_HPP_
+
+#include <string>
+
+namespace KIM
+{
+/// \brief An \ref extensible_enumeration "Extensible Enumeration" for the
+/// CollectionItemType's supported by the %KIM API.
+///
+/// The enumeration constants are contained in the COLLECTION_ITEM_TUPE
+/// namespace.
+///
+/// \sa KIM_CollectionItemType,
+/// kim_collection_item_type_module::kim_collection_item_type_type
+///
+/// \since 2.1
+class CollectionItemType
+{
+ public:
+  /// \brief Integer identifying the specific CollectionItemType represented.
+  ///
+  /// \note This should not be directly accessed and is only public for
+  /// cross-language reasons.
+  ///
+  /// \sa KIM_CollectionItemType::collectionItemTypeID,
+  /// kim_collection_item_type_module::kim_collection_item_type_type::collection_item_type_id
+  ///
+  /// \since 2.1
+  int collectionItemTypeID;
+
+  /// \brief Create an uninitialized CollectionItemType object.
+  ///
+  /// \since 2.1
+  CollectionItemType();
+
+  /// \brief Create a CollectionItemType object with the specified id.
+  ///
+  /// \note This should not be used directly.
+  ///
+  /// \since 2.1
+  CollectionItemType(int const id);
+
+  /// \brief Create a CollectionItemType object corresponding to the provided
+  /// string.  If the string does not match one of the values defined by the
+  /// %KIM API, then an "unknown" object is generated.
+  ///
+  /// \sa KIM_CollectionItemType_FromString,
+  /// kim_collection_item_type_module::kim_from_string
+  ///
+  /// \since 2.1
+  CollectionItemType(std::string const & str);
+
+  /// \brief Determines if the object is a quantity known to the %KIM API.
+  ///
+  /// CollectionItemType's known to the %KIM API are found in the
+  /// COLLECTION_TIME_TYPE namespace.
+  ///
+  /// \sa KIM_CollectionItemType_Known,
+  /// kim_collection_item_type_module::kim_known
+  ///
+  /// \since 2.1
+  bool Known() const;
+
+  /// \brief Compares CollectionItemType objects for equality.
+  ///
+  /// \note Not all "unknown" objects are equal.
+  ///
+  /// \sa KIM_CollectionItemType_Equal,
+  /// kim_collection_item_type_module::operator(.eq.)
+  ///
+  /// \since 2.1
+  bool operator==(CollectionItemType const & rhs) const;
+
+  /// \brief Compares CollectionItemType objects for inequality.
+  ///
+  /// \note It is possible for two "unknown" objects to be not equal.
+  ///
+  /// \sa KIM_CollectionItemType_NotEqual,
+  /// kim_collection_item_type_module::operator(.ne.)
+  ///
+  /// \since 2.1
+  bool operator!=(CollectionItemType const & rhs) const;
+
+  /// \brief Converts the object to a string.
+  ///
+  /// \return A string object representing the CollectionItemType object.
+  ///
+  /// \note If the CollectionItemType object does not correspond to a value
+  /// defined by the %KIM API, then the string "unknown" is returned.
+  ///
+  /// \sa KIM_CollectionItemType_ToString,
+  /// kim_collection_item_type_module::kim_to_string
+  ///
+  /// \since 2.1
+  std::string const & ToString() const;
+};  // class CollectionItemType
+
+/// \brief Contains the enumeration constants and the discovery routines for
+/// the CollectionItemType \ref extensible_enumeration "Extensible Enumeration".
+namespace COLLECTION_ITEM_TYPE
+{
+/// \brief The standard \c modelDriver CollectionItemType.
+///
+/// The item is a model driver.
+///
+/// \sa KIM_COLLECTION_ITEM_TPYE_modelDriver,
+/// kim_collection_item_type_module::kim_collection_item_type_model_driver
+///
+/// \since 2.1
+extern CollectionItemType const modelDriver;
+
+/// \brief The standard \c model CollectionItemType.
+///
+/// The item is a model.
+///
+/// \sa KIM_COLLECTION_ITEM_TPYE_model,
+/// kim_collection_item_type_module::kim_collection_item_type_model
+///
+/// \since 2.1
+extern CollectionItemType const model;
+
+/// \brief The standard \c simulatorModel CollectionItemType.
+///
+/// The item is a simulator model.
+///
+/// \sa KIM_COLLECTION_ITEM_TPYE_simulatorModel,
+/// kim_collection_item_type_module::kim_collection_item_type_simulator_model
+///
+/// \since 2.1
+extern CollectionItemType const simulatorModel;
+
+
+/// \brief Get the number of standard CollectionItemType's defined by the %KIM
+/// API.
+///
+/// \param[out] numberOfCollectionItemTypes The number of standard
+///             CollectionItemType's defined by the %KIM API.
+///
+/// \sa KIM_COLLECTION_ITEM_TYPE_GetNumberOfCollectionItemTypes,
+/// kim_collection_item_types_module::kim_get_number_of_collection_item_types
+///
+/// \since 2.1
+void GetNumberOfCollectionItemTypes(int * const numberOfCollectionItemTypes);
+
+/// \brief Get the identity of each defined standard CollectionItemType.
+///
+/// \param[in]  index Zero-based index uniquely labeling each defined standard
+///             CollectionItemType.  This index ordering is only guaranteed to
+///             be stable during the lifetime of the current process.
+/// \param[out] collectionItemType The CollectionItemType object associated with
+///             \c index.
+///
+/// \return \c true if `index < 0` or `index >= numberOfCollectionItemTypes`.
+/// \return \c false otherwise.
+///
+/// \sa KIM_COLLECTION_ITEM_TYPE_GetCollectionItemType,
+/// kim_collection_item_type_module::kim_get_collection_item_type
+///
+/// \since 2.1
+int GetCollectionItemType(int const index,
+                          CollectionItemType * const collectionItemType);
+
+/// \brief Structure provided for use with std::map.
+///
+/// \since 2.1
+struct Comparator
+{
+  /// \brief Provides an (logically unmeaningful) ordering for
+  /// CollectionItemType objects so that they can be stored in a std::map.
+  ///
+  /// \since 2.1
+  bool operator()(CollectionItemType const & a,
+                  CollectionItemType const & b) const
+  {
+    return a.collectionItemTypeID < b.collectionItemTypeID;
+  }
+};  // struct Comparator
+}  // namespace COLLECTION_ITEM_TYPE
+}  // namespace KIM
+
+#endif  // KIM_COLLECTION_ITEM_TYPE_HPP_
---
+++ cpp/include/KIM_Collections.hpp
@@ -0,0 +1,218 @@
+//
+// CDDL HEADER START
+//
+// The contents of this file are subject to the terms of the Common Development
+// and Distribution License Version 1.0 (the "License").
+//
+// You can obtain a copy of the license at
+// http://www.opensource.org/licenses/CDDL-1.0.  See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// When distributing Covered Code, include this CDDL HEADER in each file and
+// include the License file in a prominent location with the name LICENSE.CDDL.
+// If applicable, add the following below this CDDL HEADER, with the fields
+// enclosed by brackets "[]" replaced with your own identifying information:
+//
+// Portions Copyright (c) [yyyy] [name of copyright owner]. All rights reserved.
+//
+// CDDL HEADER END
+//
+
+//
+// Copyright (c) 2016--2019, Regents of the University of Minnesota.
+// All rights reserved.
+//
+// Contributors:
+//    Ryan S. Elliott
+//
+
+//
+// Release: This file is part of the kim-api.git repository.
+//
+
+
+#ifndef KIM_COLLECTIONS_HPP_
+#define KIM_COLLECTIONS_HPP_
+
+namespace KIM
+{
+// Forward declarations
+class LogVerbosity;
+class Collection;
+class CollectionItemType;
+class CollectionsImplementation;
+
+/// \brief Provides the interface to the %KIM API Collections and is meant to
+/// be used by simulators.
+///
+/// \sa KIM_Collections, kim_collections_module::kim_collections_handle_type
+///
+/// \since 2.1
+class Collections
+{
+ public:
+  /// \brief Create a new %KIM API Collections object.
+  ///
+  /// \param[out] collections Pointer to the newly created Collections object.
+  ///
+  /// \return \c false
+  ///
+  /// \sa KIM_Collections_Create, kim_collections_module::kim_collections_create
+  ///
+  /// \since 2.1
+  static int Create(Collections ** const collections);
+
+  /// \brief Destroy a previously Collections::Create'd object.
+  ///
+  /// \param[inout] collections Pointer to the Collections object.
+  ///
+  /// \pre \c collections points to a previously created %KIM API Collections
+  ///      object.
+  ///
+  /// \post `collections == NULL`.
+  ///
+  /// \sa KIM_Collections_Destroy,
+  /// kim_collections_module::kim_collections_destroy
+  ///
+  /// \since 2.1
+  static void Destroy(Collections ** const collections);
+
+  /// @@@ add docs
+  ///
+  /// \since 2.1
+  int GetTypeOfItem(std::string const & itemName,
+                    CollectionItemType * const typeOfItem) const;
+
+  /// @@@ add docs
+  ///
+  /// \since 2.1
+  int GetItem(CollectionItemType const itemType,
+              std::string const & itemName,
+              std::string const ** const path,
+              int * const metadataExtent,
+              Collection * const collection) const;
+
+  /// @@@ add docs
+  ///
+  /// pointers are valid until next call of GetItemMetadata
+  /// \since 2.1
+  int GetItemMetadata(CollectionItemType const itemType,
+                      std::string const & itemName,
+                      int const index,
+                      std::string const ** const metadataID,
+                      std::string const ** const metadataString) const;
+
+  /// @@@ add docs
+  ///
+  /// @@@ return a colon separated list of names
+  ///
+  /// \since 4.1
+  int GetItemNamesByType(CollectionItemType const itemType,
+                         std::string const ** const itemNames) const;
+
+  /// @@@ add docs
+  ///
+  /// @@@ return a colon separated list of names
+  ///
+  /// \since 2.1
+  int GetItemNamesByCollectionAndType(
+      Collection const collection,
+      CollectionItemType const itemType,
+      std::string const ** const itemNames) const;
+
+  /// @@@ add docs
+  ///
+  /// \since 2.1
+  int GetItemByCollectionAndType(Collection const collection,
+                                 CollectionItemType const itemType,
+                                 std::string const & itemName,
+                                 std::string const ** const path,
+                                 int * const metadataExtent) const;
+
+  /// @@@ add docs
+  ///
+  /// pointers are valid until next call of GetItemMetadata
+  /// \since 2.1
+  int GetItemMetadataByCollectionAndType(
+      Collection const collection,
+      CollectionItemType const itemType,
+      std::string const & itemName,
+      int const index,
+      std::string const ** const metadataID,
+      std::string const ** const metadataString) const;
+
+  /// @@@ add docs
+  ///
+  /// \since 2.1
+  void GetProjectNameAndSemVer(std::string const ** const projectName,
+                               std::string const ** const semVer) const;
+
+  /// @@@ add docs
+  ///
+  /// \since 2.1
+  int GetEnvironmentVariableName(CollectionItemTYpe const itemType,
+                                 std::string const ** const name) const;
+
+  /// @@@ add docs
+  ///
+  /// \since 2.1
+  int GetConfigurationFileEnvironmentVariable(
+      std::string const ** const name, std::string const ** const value) const;
+
+  /// @@@ add docs
+  ///
+  /// \since 2.1
+  int GetConfigurationFilePath(std::string const ** const filePath) const;
+
+  /// @@@ add docs
+  ///
+  /// @@@ return colon separated list of paths
+  ///
+  /// \since 2.1
+  int GetDirectories(Collection const collection,
+                     CollectionItemType const itemType,
+                     std::string const ** const directories) const;
+
+  /// \brief Set the identity of the Log object associated with the Collections
+  /// object.
+  ///
+  /// \param[in] logID String identifying the Collections object's Log object.
+  ///
+  /// \sa KIM_Collections_SetLogID, kim_Collections_module::kim_set_log_id
+  ///
+  /// \since 2.1
+  void SetLogID(std::string const & logID);
+
+  /// \brief Push a new LogVerbosity onto the Collections object's Log object
+  /// verbosity stack.
+  ///
+  /// \param[in] logVerbosity A LogVerbosity value.
+  ///
+  /// \sa KIM_Collections_PushLogVerbosity,
+  /// kim_collections_module::kim_push_log_verbosity
+  ///
+  /// \since 2.1
+  void PushLogVerbosity(LogVerbosity const logVerbosity);
+
+  /// \brief Pop a LogVerbosity from the Collections object's Log object
+  /// verbosity stack.
+  ///
+  /// \sa KIM_Collections_PopLogVerbosity,
+  /// kim_collections_module::kim_pop_log_verbosity
+  ///
+  /// \since 2.1
+  void PopLogVerbosity();
+
+ private:
+  // do not allow copy constructor or operator=
+  Collections(Collections const &);
+  void operator=(Collections const &);
+
+  Collections();
+  ~Collections();
+
+  CollectionsImplementation * pimpl;
+};  // class Collections
+}  // namespace KIM
+
+#endif  // KIM_COLLECTIONS_HPP_
