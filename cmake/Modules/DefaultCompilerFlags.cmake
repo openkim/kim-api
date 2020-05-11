c098fa051bfc76d790b00e8cdc3dc62e793e3da1
Alexander Stukowski <stukowski@mm.tu-darmstadt.de>
2020-05-11 10:25:42 +0200

    Don't use the -pie linker flag when generating executables for MinGW platform

--- cmake/Modules/DefaultCompilerFlags.cmake
+++ cmake/Modules/DefaultCompilerFlags.cmake
@@ -46,7 +46,9 @@ if(KIM_API_ENABLE_COVERAGE)
 endif()
 if(NOT CMAKE_C_COMPILER_ID STREQUAL Intel
     AND
-    NOT CMAKE_C_COMPILER_ID STREQUAL AppleClang)
+    NOT CMAKE_C_COMPILER_ID STREQUAL AppleClang
+    AND # Note: -pie linker flag breaks generated executables when using MinGW toolchain.
+    NOT MINGW)
   set(KIM_API_EXE_LINKER_FLAGS "${KIM_API_EXE_LINKER_FLAGS} -pie" CACHE STRING "KIM API linker flags")
 endif()
 #
