188a2f4456158d11578b0cc4e59e7070ab8aed46
Ryan S. Elliott <relliott@umn.edu>
2020-07-21 09:03:58 -0500

    Update copyright dates to 2020

--- examples/model-drivers/ex_model_driver_P_LJ/ex_model_driver_P_LJ.f90
+++ examples/model-drivers/ex_model_driver_P_LJ/ex_model_driver_P_LJ.f90
@@ -19,7 +19,7 @@
 !
 
 !
-! Copyright (c) 2013--2019, Regents of the University of Minnesota.
+! Copyright (c) 2013--2020, Regents of the University of Minnesota.
 ! All rights reserved.
 !
 ! Contributors:
