788f6fcd7842a25768b0c6d925ad025fa64e70d4
Ryan S. Elliott <relliott@umn.edu>
2021-01-04 15:20:55 -0600

    Update copyright dates to 2021

--- c/src/KIM_ChargeUnit_c.cpp
+++ c/src/KIM_ChargeUnit_c.cpp
@@ -21,7 +21,7 @@
 //
 
 //
-// Copyright (c) 2016--2020, Regents of the University of Minnesota.
+// Copyright (c) 2016--2021, Regents of the University of Minnesota.
 // All rights reserved.
 //
 // Contributors:
