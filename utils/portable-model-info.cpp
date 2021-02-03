98c32976bcf9a897971223a7b465178d26f95109
Ryan S. Elliott <relliott@umn.edu>
2021-02-03 11:04:22 -0600

    Fix memory leaks in portable-model-info.cpp

--- utils/portable-model-info.cpp
+++ utils/portable-model-info.cpp
@@ -65,7 +65,7 @@ int main(int argc, char * argv[])
   if ((argc != 2) || (std::string(argv[1]) == ""))
   {
     usage(argv[0]);
-    exit(1);
+    return 1;
   }
 
   if ((argc == 2) && (std::string(argv[1]) == "--version"))
@@ -340,6 +340,10 @@ int main(int argc, char * argv[])
   edn << "} ";
 
   edn << "}";
+
+  // destroy the objects
+  error = mdl->ComputeArgumentsDestroy(&computeArguments);
+  KIM::Model::Destroy(&mdl);
 
   edn::EdnNode modelInfo;
   try
